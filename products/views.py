3410ed9c41534c9213d59746c36c92b854a5210f
Korollayre <mbuinichienko@mail.ru>
2021-07-04 15:35:00 +0300

    Work with media

--- products/views.py
+++ products/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 import json
 
 
-with open('products/fixtures/goods.json', 'r', encoding='utf-8') as json_file:
+with open('products/fixtures/goods.json', 'r') as json_file:
     goods = json.load(json_file)
+
+with open('products/fixtures/categories.json', 'r') as json_file:
+    categories = json.load(json_file)
 
 # Create your views here.
 
@@ -16,6 +19,7 @@ def index(request):
 def products(request):
     context = {
         'title': 'GeekShop - Products',
-        'products': goods
+        'products': goods,
+        'categories': categories,
     }
     return render(request, 'products/products.html', context)
