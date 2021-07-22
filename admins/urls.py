8869d78e713c4fdc9da0c0db330dd5c5accf1d8b
Korollayre <mbuinichienko@mail.ru>
2021-07-22 12:20:47 +0300

    Added CBV UserListView and UserCreateView

--- admins/urls.py
+++ admins/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
-from admins.views import index, admin_users, admin_users_create, admin_users_update, admin_users_remove, \
+from admins.views import index, UserListView, UserCreateView, admin_users_update, admin_users_remove, \
     admin_product_category, admin_product_category_create, admin_product_category_remove, admin_product_category_update
 
 app_name = 'admins'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('users/', admin_users, name='admin_users'),
-    path('users/create/', admin_users_create, name='admin_users_create'),
+    path('users/', UserListView.as_view(), name='admin_users'),
+    path('users/create/', UserCreateView.as_view(), name='admin_users_create'),
     path('users/update/<int:pk>/', admin_users_update, name='admin_users_update'),
     path('users/remove/<int:pk>/', admin_users_remove, name='admin_users_remove'),
     path('categories/', admin_product_category, name='admin_product_category'),
